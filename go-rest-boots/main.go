593cd8c8019183748b9f3020c1c3fc859484cb2e
craigmartin <craig@mglee.com>
2014-08-03 13:49:37 -0700

    adding go workspace

---
+++ go-rest-boots/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+        "encoding/json"
+        "flag"
+        "fmt"
+        "log"
+        "net/http"
+
+        "github.com/gorilla/mux"
+)
+
+// Error response contains everything we need to use http.Error
+
+
